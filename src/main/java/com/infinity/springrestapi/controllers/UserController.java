dcd2b39ed7b6af1e143c47b09a64f76002fab356
Ryan Mark <ryanmarkdelosreyes04@gmail.com>
2025-09-22 22:27:21 +0800

    created dynamic routes and setup the correct status code for users

---
+++ src/main/java/com/infinity/springrestapi/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.infinity.springrestapi.controllers;
+
+import com.infinity.springrestapi.entities.User;
+import com.infinity.springrestapi.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
+    }
+}
